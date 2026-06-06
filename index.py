50920316b3687183887cdba937582f2ed17ece43
Nisha R S <nisha021rs@gmail.com>
2026-06-06 22:08:03 +0530

    Checked if the files are same.

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
-print(train.head())
-print(test.head())
+# print(train.head())
+# print(test.head())
+print(train.info())
