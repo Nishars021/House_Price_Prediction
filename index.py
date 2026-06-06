955dafbcae83a8355f641990310101c8777a372a
Nisha R S <nisha021rs@gmail.com>
2026-06-06 23:39:37 +0530

    Performing...

--- index.py
+++ index.py
@@ -6,13 +6,27 @@ test = pd.read_csv("test.csv")
 # print(train.info())
 # print(test.info())
 # print(train.describe())
-missing = train.isnull().sum()
-# print(missing)
-print(missing[missing > 0])
+# missing = train.isnull().sum()
+# # print(missing)
+# print(missing[missing > 0])
 
 #Data cleaning
-# train["LotFrontage"] = train["LotFrontage"].fillna((train["LotFrontage"].mean))
-# missing = train["LotFrontage"].isnull().sum()
-# print(missing)
+#LotFrontage
+train["LotFrontage"] = train["LotFrontage"].fillna((train["LotFrontage"].mean))
+print(train["LotFrontage"].isnull().sum())
 
+#Alley
+train["Alley"] = train["Alley"].fillna(train["Alley"].mode)
+print(train["Alley"].isnull().sum())
 
+#MasVnrType
+train["MasVnrType"] = train["MasVnrType"].fillna(train["MasVnrType"].mode)
+print(train["MasVnrType"].isnull().sum())
+
+#BsmtQual 
+train["BsmtQual"] = train["BsmtQual"].fillna(train["BsmtQual"].mode)
+print(train["BsmtQual"].isnull().sum())
+
+#BsmtCond
+train["BsmtCond"] = train["BsmtCond"].fillna(train["BsmtCond"].mode)
+print(train["BsmtCond"].isnull().sum())
